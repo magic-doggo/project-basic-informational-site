62b6156baada26d4236d889945e4c9860456dc79
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-05-25 10:29:33 +0000

    Attempt rewriting app using express the app works the same, but I am not sure if using fs.readFile defies the point of using express in the first place

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const express = require('express');
+const app = express();
 
 
-http.createServer(function (req, res) {
-    let myUrl = url.parse(req.url, true);
-    let filename;
-    if (myUrl.pathname === '/') { filename = "./index.html" } //(|| myUrl.pathname === '') seems to not be needed, pathname is set to "/" even if empty?
-    else if (myUrl.pathname === '/contact-me' || myUrl.pathname === '/about') {
-        filename = '.' + myUrl.pathname + '.html'
-    }
-    else filename = './404.html'
-    fs.readFile(filename, function (err, data) {
-        // if (err) {
-        //     res.writeHead(404, { 'Content-Type': 'text/html' });
-        //     return res.end('404 Not FOund');
-        // }
+app.get('/', function(req, res, next) {
+    const fileName = "index.html";
+    fs.readFile(fileName, function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
     })
-}).listen(8080)
+})
+
+app.get(['/contact-me'], function(req, res, next) {
+    const fileName = "contact-me.html";
+    fs.readFile(fileName, function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    })
+})
+
+app.get(['/about'], function(req, res, next) {
+    const fileName = "about.html";
+    fs.readFile(fileName, function (err, data) {
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    })
+})
+
+app.all(/.*/, function(req, res, next) {
+    const fileName = "404.html";
+    fs.readFile(fileName, function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('express app on port', PORT))
+
