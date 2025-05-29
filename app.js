f40e9e92ad8aa116c66e255d6f567b3c4904f4ad
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-05-29 06:16:42 +0000

    Properly use express with sendFile instead of  fs.readFile

--- app.js
+++ app.js
@@ -1,45 +1,30 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
 const express = require('express');
 const app = express();
 
 
 app.get('/', function(req, res, next) {
-    const fileName = "index.html";
-    fs.readFile(fileName, function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    })
+    const fileName = "/index.html";
+    res.sendFile(__dirname + fileName)
+    // fs.readFile(fileName, function (err, data) {
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write(data);
+    //     return res.end();
+    // })
 })
 
 app.get(['/contact-me'], function(req, res, next) {
-    const fileName = "contact-me.html";
-    fs.readFile(fileName, function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    })
+    const fileName = "/contact-me.html";
+    res.sendFile(__dirname + fileName);
 })
 
 app.get(['/about'], function(req, res, next) {
-    const fileName = "about.html";
-    fs.readFile(fileName, function (err, data) {
-
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    })
+    const fileName = "/about.html";
+    res.sendFile(__dirname + fileName);
 })
 
 app.all(/.*/, function(req, res, next) {
-    const fileName = "404.html";
-    fs.readFile(fileName, function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    })
+    const fileName = "/404.html";
+    res.sendFile(__dirname + fileName);
 })
 
 const PORT = process.env.PORT || 3000;
